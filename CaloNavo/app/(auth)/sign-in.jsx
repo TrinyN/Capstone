ed6e926d15547f680c91dbf3544b7c436a4dd097
TrinyN <triny.nguyen@g.fmarion.edu>
2024-08-29 21:08:26 -0400

    sign in page and welcome page edits

--- CaloNavo/app/(auth)/sign-in.jsx
+++ CaloNavo/app/(auth)/sign-in.jsx
@@ -1,16 +1,14 @@
 // import FormField from "../../components/FormField";
-import { ScrollView, TouchableOpacity, Text, Image, View } from 'react-native';
+import { ScrollView, TextInput, TouchableOpacity, Text, Image, View } from 'react-native';
 import { SafeAreaView } from "react-native-safe-area-context";
-import styles from '../font-styles';
+import styles from '../styles';
 
 const SignIn = () => {
-
-
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.viewContainer}>
 
-                <View style={{ flex: 1 }}>
+                <View style={{ flex: 1, marginTop: 140 }}>
                     <Text style={styles.titleText}>
                         Glad to see you again!
                     </Text>
@@ -20,18 +18,36 @@ const SignIn = () => {
                     <Text style={styles.defaultText}>
                         Email:
                     </Text>
+                    <TextInput style={{ color: '#F2F4F3', backgroundColor: 'rgba(97, 98, 131, 0.2)', borderRadius: 8, fontSize:16, paddingLeft:15}}>
+
+
+                    </TextInput>
+
                     <Text style={styles.defaultText}>
                         Password:
                     </Text>
+
+                    <TextInput>
+
+                    </TextInput>
                 </View>
                 <View style={{ flex: 1 }}>
 
                     <TouchableOpacity style={styles.button}>
                         <Text style={styles.buttonText}>Log In</Text>
                     </TouchableOpacity>
+                    <Text style={styles.defaultText}>
+                        or continue with
+                    </Text>
                     <TouchableOpacity style={styles.button}>
                         <Text style={styles.buttonText}>Google</Text>
                     </TouchableOpacity>
+                    <Text style={styles.defaultWhiteText}>
+                        By clicking continue, you agree to our Terms of Service and Privacy Policy
+                    </Text>
+                    <Text style={styles.defaultWhiteText}>
+                        Don't have an account? Sign Up
+                    </Text>
                 </View>
             </View>
 
--- CaloNavo/app/index.jsx
+++ CaloNavo/app/index.jsx
@@ -3,7 +3,7 @@ import { StyleSheet, TouchableOpacity, Button, ScrollView, Text, Image, View } f
 import { SafeAreaView } from "react-native-safe-area-context";
 import { CustomButton } from "../components/CustomButton";
 import { Redirect, router } from 'expo-router';
-import styles from './font-styles';
+import styles from './styles';
 
 
 export default function App() {
@@ -11,7 +11,7 @@ export default function App() {
         <SafeAreaView style={[styles.container]}>
             <View style={styles.viewContainer}>
                 <View style={{ flex: 4, marginTop: 30 }}>
-                    <Text style={[styles.welcomeText, styles.defaultText, { paddingTop: 90 }]}>
+                    <Text style={[styles.welcomeText, {fontSize: 24, textAlign: 'center', paddingTop: 90 }]}>
                         Welcome to
                     </Text>
                     <Text style={styles.welcomeText}>
@@ -19,7 +19,7 @@ export default function App() {
                     </Text>
                 </View>
                 <View style={{ flex: 1, justifyContent: 'flex-end' }}>
-                    <Text style={[styles.defaultText, { fontWeight: '300', color: 'white', paddingLeft: 50, paddingRight: 50, textAlign: 'center' }]}>
+                    <Text style={[styles.defaultText, { fontWeight: '300', color: '#F2F4F3', paddingLeft: 50, paddingRight: 50, textAlign: 'center' }]}>
                         Here to help navogate your way to a better diet
                     </Text>
                 </View>
