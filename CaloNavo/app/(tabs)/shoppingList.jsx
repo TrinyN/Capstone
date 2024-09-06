394f74fdec25c03fb8a498196cc4b24f8e949032
TrinyN <triny.nguyen@g.fmarion.edu>
2024-09-05 21:35:45 -0400

    made navigation tabs and report UI

---
+++ CaloNavo/app/(tabs)/shoppingList.jsx
@@ -0,0 +1,29 @@
+import { TextInput, TouchableOpacity, Text, View, ScrollView } from 'react-native';
+import { SafeAreaView } from "react-native-safe-area-context";
+import styles from '../styles';
+import { StatusBar } from 'expo-status-bar';
+import { router } from 'expo-router';
+import Feather from "react-native-vector-icons/Feather";
+
+const ShoppingList = () => {
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+
+                <View style={[styles.viewContainer, { flex: 1 }]}>
+
+                    <View style={{ marginTop: 70, flex: 1 }}>
+                        <Text style={[styles.titleText, { paddingBottom: 10 }]}>
+                            Shopping List
+                        </Text>
+
+                    </View>
+                </View>
+
+
+            </ScrollView>
+        </SafeAreaView>
+    )
+}
+export default ShoppingList;
