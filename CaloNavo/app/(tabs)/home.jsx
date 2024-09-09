8d9412c255b378f4d4682e0c0718f7ed6c96969a
TrinyN <triny.nguyen@g.fmarion.edu>
2024-09-09 19:04:58 -0400

    Update home.jsx

--- CaloNavo/app/(tabs)/home.jsx
+++ CaloNavo/app/(tabs)/home.jsx
@@ -33,104 +33,107 @@ const Home = () => {
 
                     {/* View to contain all other major elements, like the frames for each quick tool */}
                     <View style={{ flex: 5, paddingBottom: 50 }}>
-                        
+
                         {/* Wide View Frame for Daily Diet Progress */}
                         <View style={styles.viewHomeFrameWide}>
-                            <Text style={ [styles.frameTextWhite, {paddingVertical: 5}]}>
+                            <Text style={[styles.frameTextWhite, { paddingVertical: 5 }]}>
                                 Your Daily Diet Progress:
                             </Text>
-                            
+
                             {/* View that holds the progress bars and their labels */}
-                            <View style={ { flexDirection: 'row', alignItems: 'center', paddingHorizontal: 10 } }>
-                                {/* Left column for Bar Labels */}
-                                <View style={ { flexDirection: 'column', marginRight: 25, marginBottom: 5 } }>
-                                    <Text style={ [styles.homeProgressLabelText, {color: '#CB9CF2'} ]}>
+                            <View style={{ flexDirection: 'column', paddingBottom: 10 }}>
+                                <View style={{ flexDirection: 'row', }} >
+                                    <Text style={[styles.homeProgressLabelText, { color: '#CB9CF2', flex: 1 }]}>
                                         Calories
                                     </Text>
-                                    <Text style={ [styles.homeProgressLabelText, {color: '#80FF72'} ]}>
-                                        Carbs
-                                    </Text>
-                                    <Text style={ [styles.homeProgressLabelText, {color: '#7EE8FA'} ]}>
-                                        Proteins
-                                    </Text>
-                                    <Text style={ [styles.homeProgressLabelText, {color: '#FFF07C'} ]}>
-                                        Fats
-                                    </Text>
-                                </View>
-
-                                {/* NOTE: I tried making styles for Progress Bars, but they cause the progress to glitch out */}
-                                {/* Right column for Progress Bars. NOTE: Each Progress bar contains their own text values too */}
-                                <View style={ { flexDirection: 'column'} }>
-                                    <Progress.Bar 
-                                        progress={0.5} 
+                                    <Progress.Bar
+                                        progress={0.5}
                                         color={'#CB9CF2'}
                                         backgroundColor={'#0E1116'}
-                                        height={20} 
-                                        width={200} 
-                                        borderRadius={15} 
-                                        marginBottom={5}> 
-                                        <Text style={ [styles.homeProgressBarTextLeft, {position:'absolute', flex:0}] }>
+                                        height={20}
+                                        width={200}
+                                        borderRadius={15}
+                                        style={{ marginBottom: 5 }}>
+                                        <Text style={[styles.homeProgressBarTextLeft, { position: 'absolute', flex: 0 }]}>
                                             1400
                                         </Text>
                                     </Progress.Bar>
-                                    <Progress.Bar 
-                                        progress={0.5} 
-                                        color={'#80FF72'} 
+                                </View>
+
+                                <View style={{ flexDirection: 'row' }} >
+                                    <Text style={[styles.homeProgressLabelText, { color: '#80FF72', flex: 1 }]}>
+                                        Carbs
+                                    </Text>
+                                    <Progress.Bar
+                                        progress={0.5}
+                                        color={'#80FF72'}
                                         backgroundColor={'#0E1116'}
-                                        height={20} 
-                                        width={200} 
-                                        borderRadius={15} 
-                                        marginBottom={5}> 
-                                        <Text style={ [styles.homeProgressBarTextLeft, {position:'absolute', flex:0}] }>
+                                        height={20}
+                                        width={200}
+                                        borderRadius={15}
+                                        style={{ marginBottom: 5 }}>
+                                        <Text style={[styles.homeProgressBarTextLeft, { position: 'absolute', flex: 0 }]}>
                                             250
-                                        </Text> 
-                                    </Progress.Bar>
-                                    <Progress.Bar 
-                                        progress={0.5} 
-                                        color={'#7EE8FA'} 
-                                        backgroundColor={'#0E1116'}
-                                        height={20} 
-                                        width={200} 
-                                        borderRadius={15} 
-                                        marginBottom={5}> 
-                                        <Text style={ [styles.homeProgressBarTextLeft, {position:'absolute', flex:0}] }>
-                                            100
-                                        </Text> 
-                                    </Progress.Bar>
-                                    <Progress.Bar 
-                                        progress={0.5} 
-                                        color={'#FFF07C'} 
-                                        backgroundColor={'#0E1116'}
-                                        height={20} 
-                                        width={200} 
-                                        borderRadius={15} 
-                                        marginBottom={5}> 
-                                        <Text style={ [styles.homeProgressBarTextLeft, {position:'absolute', flex:0}] }>
-                                            15
-                                        </Text> 
+                                        </Text>
                                     </Progress.Bar>
                                 </View>
 
-                            </View>
+                                <View style={{ flexDirection: 'row' }} >
+                                    <Text style={[styles.homeProgressLabelText, { color: '#7EE8FA', flex: 1 }]}>
+                                        Proteins
+                                    </Text>
+                                    <Progress.Bar
+                                        progress={0.5}
+                                        color={'#7EE8FA'}
+                                        backgroundColor={'#0E1116'}
+                                        height={20}
+                                        width={200}
+                                        borderRadius={15}
+                                        style={{ marginBottom: 5 }}>
+                                        <Text style={[styles.homeProgressBarTextLeft, { position: 'absolute', flex: 0 }]}>
+                                            100
+                                        </Text>
+                                    </Progress.Bar>
+                                </View>
 
+                                <View style={{ flexDirection: 'row' }} >
+                                    <Text style={[styles.homeProgressLabelText, { color: '#FFF07C', flex: 1 }]}>
+                                        Fats
+                                    </Text>
+                                    <Progress.Bar
+                                        progress={0.5}
+                                        color={'#FFF07C'}
+                                        backgroundColor={'#0E1116'}
+                                        height={20}
+                                        width={200}
+                                        borderRadius={15}
+                                        style={{ marginBottom: 5 }}>
+                                        <Text style={[styles.homeProgressBarTextLeft, { position: 'absolute', flex: 0 }]}>
+                                            15
+                                        </Text>
+                                    </Progress.Bar>
+                                </View>
+                            </View>
                         </View>
 
                         {/* Two FlatLists to hold two separate columns of quick tool frames, with frames of different heights */}
 
-                        <View style={ [styles.viewFrameContainer, { flexDirection: 'row' }]}>
+                        <View style={[styles.viewFrameContainer, { flexDirection: 'row' }]}>
 
                             {/* FlatList for left column, containing water goal display frame */}
-                            <View style={ [styles.containerColumn, { marginRight: 15 }] }>
+                            <View style={[styles.containerColumn, { marginRight: 15 }]}>
 
                                 {/* Water Goal Display Frame */}
-                                <View style={styles.viewHomeFrameTall}>
-                                    
+                                <View style={[styles.viewHomeFrameTall, {alignContent: 'center'}]}>
+
                                     {/* TouchableOpacity to make frame contents a button */}
                                     <TouchableOpacity
-                                        style={ { alignItems:'center',
-                                                    height: '100%',
-                                                    width: '100%'} } 
-                                        onPress={() => router.push('/tracker')}>
+                                        style={{
+                                            alignItems: 'center',
+                                            height: '100%',
+                                            width: '100%'
+                                        }}
+                                        onPress={() => router.push('/tracker')}> 
                                         <Text style={styles.frameTextWhite}>
                                             Staying {'\n'}
                                             Hydrated?
@@ -140,37 +143,41 @@ const Home = () => {
                                         <Image
                                             source={require('../../assets/images/water-bottle-1.png')}
                                             style={{
+                                                paddingVertical: 20,
                                                 height: 200,
                                                 width: 125,
                                                 resizeMode: 'contain',
+                                                // alignSelf: 'center',
                                                 marginLeft: 10       // May have to be changed to make slightly off-left instead of centered
                                             }}
                                         />
-                                    </TouchableOpacity>               
+                                    </TouchableOpacity>
 
                                 </View>
                             </View>
 
                             {/* FlatList for right column, containing Quick Track and Notes frames */}
-                            <View style={ [ styles.containerColumn, { marginLeft: 15 } ] }>
+                            <View style={[styles.containerColumn, { marginLeft: 15 }]}>
 
                                 {/* Quick Track Frame */}
-                                <View style={ [ styles.viewHomeFrameNormal, { marginBottom: 20 } ] }>
-                                    
+                                <View style={[styles.viewHomeFrameNormal, { marginBottom: 20 }]}>
+
                                     {/* Touchable Opacity to make entire frame a button */}
                                     {/* TODO: Add popup for quick tracker */}
-                                    <TouchableOpacity 
-                                        style={ { alignItems:'center',
-                                                    height: '100%',
-                                                    width: '100%'} } 
+                                    <TouchableOpacity
+                                        style={{
+                                            alignItems: 'center',
+                                            height: '100%',
+                                            width: '100%'
+                                        }}
                                         onPress={() => router.push('/tracker')}>
                                         <Text style={styles.frameTextWhite}>
                                             Quick Track
                                         </Text>
                                         {/* How to fill pruple with dark plus (~Figma)??? */}
                                         {/* Issue: SVG (including stroke and fill) no long supported for feather */}
-                                        <Feather name='plus-circle' 
-                                            size={55} 
+                                        <Feather name='plus-circle'
+                                            size={55}
                                             color={'#CB9CF2'}
                                         />
                                     </TouchableOpacity>
@@ -179,17 +186,19 @@ const Home = () => {
 
                                 {/* Take Notes Frame */}
                                 <View style={styles.viewHomeFrameNormal}>
-                                    
+
                                     {/* Touchable Opacity for making entire Notes frame a button */}
-                                    <TouchableOpacity 
-                                        style={ { alignItems:'center',
-                                                    height: '100%',
-                                                    width: '100%'} } 
+                                    <TouchableOpacity
+                                        style={{
+                                            alignItems: 'center',
+                                            height: '100%',
+                                            width: '100%'
+                                        }}
                                         onPress={() => router.push('/home')}>
                                         <Text style={styles.frameTextWhite}>
                                             Notes
                                         </Text>
-                                        <Feather name='edit-3' size={55} color={'#CB9CF2'}/>
+                                        <Feather name='edit-3' size={55} color={'#CB9CF2'} />
                                     </TouchableOpacity>
 
                                 </View>
