a80cbad644b85b0d58100501cd569738a4ed10db
TrinyN <triny.nguyen@g.fmarion.edu>
2024-10-05 17:23:25 -0400

    Update sign-up-3.jsx

--- app/(auth)/sign-up-3.jsx
+++ app/(auth)/sign-up-3.jsx
@@ -1,10 +1,9 @@
-import { TextInput, TouchableOpacity, Text, View, ScrollView } from 'react-native';
-import { SafeAreaView } from "react-native-safe-area-context";
+import { TextInput, TouchableOpacity, Text, View } from 'react-native';
 import styles from '../styles';
 import { router } from 'expo-router';
-import Feather from "react-native-vector-icons/Feather";
 import { useState } from 'react';
 import CustomDropdown from '../components/CustomDropdown';
+import CustomScreen from '../components/CustomScreen';
 
 // REFACTORING FLAGGING - Needs a Component
 // 1. Screen component - sign up 2 and 3 and report all same
@@ -37,128 +36,109 @@ const SignUp3 = () => {
     ]);
 
     return (
-        <SafeAreaView style={styles.container}>
-            <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <CustomScreen
+            title='Your diet goals ...'
+            info='Please fill in the following fields to set up your diet and goals.'
+            hasBackButton={true}
+            screenContent={
 
-                {/* Top bar holding the back button */}
-                <View style={{ marginTop: 40, marginBottom: 10, paddingLeft: 25 }}>
-                    <TouchableOpacity style={{ width: 35 }} onPress={() => router.back()}>
-                        <Feather name="chevron-left" size={35} color="#F2F4F3" />
-                    </TouchableOpacity>
-                </View>
+                // {/* View to hold all of the questions and fields */}
+                <View style={{ flex: 5, paddingBottom: 100 }}>
+                    {/* Questions */}
+                    {/* Diet Plan question and field */}
+                    <Text style={styles.defaultText}>
+                        Would you like a diet plan?
+                    </Text>
 
-                {/* The "body" of the screen, including its major elements */}
-                <View style={[styles.viewContainer, { flex: 1 }]}>
-
-                    {/* Screen header */}
-                    <View style={{ marginTop: 30, flex: 1 }}>
-                        <Text style={[styles.titleText, { paddingBottom: 10 }]}>
-                            Your diet goals ...
-                        </Text>
-                        <Text style={[styles.defaultWhiteText, { fontSize: 20 }]}>
-                            Please fill in the following fields to set up your diet and goals.
-                        </Text>
+                    <View style={[{ zIndex: 3 }]}>
+                        <CustomDropdown
+                            placeholder={''}
+                            setCustomValue={setUserDietPlanBoolean}
+                            items={dietPlanBoolean}
+                            setItems={setDietPlanBoolean}
+                        />
                     </View>
 
-                    {/* View to hold all of the questions and fields */}
-                    <View style={{ flex: 5, paddingBottom: 100 }}>
-                        {/* Questions */}
-                        {/* Diet Plan question and field */}
-                        <Text style={styles.defaultText}>
-                            Would you like a diet plan?
-                        </Text>
+                    {/* if user chose yes to a diet plan, render approriate text inputs */}
+                    {userDietPlanBoolean && (
+                        <>
+                            {/* Diet Plan Type question and options */}
+                            <Text style={styles.defaultText}>
+                                Which diet plan would you like?
+                            </Text>
 
-                        <View style={[{ zIndex: 3 }]}>
-                            <CustomDropdown
-                                placeholder={''}
-                                setCustomValue={setUserDietPlanBoolean}
-                                items={dietPlanBoolean}
-                                setItems={setDietPlanBoolean}
-                            />
-                        </View>
+                            <View style={[{ zIndex: 2 }]}>
+                                <CustomDropdown
+                                    placeholder={''}
+                                    setCustomValue={setUserDietPlan}
+                                    items={dietPlan}
+                                    setItems={setDietPlan}
+                                />
 
-                        {/* if user chose yes to a diet plan, render approriate text inputs */}
-                        {userDietPlanBoolean && (
-                            <>
-                                {/* Diet Plan Type question and options */}
-                                <Text style={styles.defaultText}>
-                                    Which diet plan would you like?
-                                </Text>
+                            </View>
 
-                                <View style={[{ zIndex: 2 }]}>
-                                    <CustomDropdown
-                                        placeholder={''}
-                                        setCustomValue={setUserDietPlan}
-                                        items={dietPlan}
-                                        setItems={setDietPlan}
-                                    />
+                            {/* Diet Name question and field */}
+                            <Text style={styles.defaultText}>
+                                Custom diet name? (optional)
+                            </Text>
+                            <TextInput style={styles.inputFieldStyle}
+                                selectionColor='#CB9CF2'
+                                placeholderTextColor='rgba(242,244,243, 0.2)'
+                                placeholder='My Diet Plan'>
+                            </TextInput>
+                        </>
+                    )}
+                    {/* Daily Calories question and field */}
+                    <Text style={styles.defaultText}>
+                        How many calories per day?
+                    </Text>
+                    <TextInput style={styles.inputFieldStyle}
+                        selectionColor='#CB9CF2'
+                        placeholderTextColor='rgba(242,244,243, 0.2)'
+                        placeholder='2500'>
+                    </TextInput>
 
-                                </View>
+                    {/* Macro Ratio question and field */}
+                    <Text style={styles.defaultText}>
+                        What macro ratio do you want?
+                    </Text>
+                    <TextInput style={styles.inputFieldStyle}
+                        selectionColor='#CB9CF2'
+                        placeholderTextColor='rgba(242,244,243, 0.2)'
+                        placeholder='35% Carb / 35% Protein / 30% Fat'>
+                    </TextInput>
 
-                                {/* Diet Name question and field */}
-                                <Text style={styles.defaultText}>
-                                    Custom diet name? (optional)
-                                </Text>
-                                <TextInput style={styles.inputFieldStyle}
-                                    selectionColor='#CB9CF2'
-                                    placeholderTextColor='rgba(242,244,243, 0.2)'
-                                    placeholder='My Diet Plan'>
-                                </TextInput>
-                            </>
-                        )}
-                        {/* Daily Calories question and field */}
-                        <Text style={styles.defaultText}>
-                            How many calories per day?
-                        </Text>
-                        <TextInput style={styles.inputFieldStyle}
-                            selectionColor='#CB9CF2'
-                            placeholderTextColor='rgba(242,244,243, 0.2)'
-                            placeholder='2500'>
-                        </TextInput>
+                    {/* Weight Goal question and field */}
+                    <Text style={styles.defaultText}>
+                        What is your weight goal?
+                    </Text>
+                    <View style={[{ zIndex: 1 }]}>
+                        <CustomDropdown
+                            placeholder={''}
+                            setCustomValue={setUserWeightGoal}
+                            items={weightGoal}
+                            setItems={setWeightGoal}
+                        />
+                    </View>
 
-                        {/* Macro Ratio question and field */}
-                        <Text style={styles.defaultText}>
-                            What macro ratio do you want?
-                        </Text>
-                        <TextInput style={styles.inputFieldStyle}
-                            selectionColor='#CB9CF2'
-                            placeholderTextColor='rgba(242,244,243, 0.2)'
-                            placeholder='35% Carb / 35% Protein / 30% Fat'>
-                        </TextInput>
+                    {/* Water Goal question and field */}
+                    <Text style={styles.defaultText}>
+                        What is your water goal?
+                    </Text>
+                    <TextInput style={styles.inputFieldStyle}
+                        selectionColor='#CB9CF2'
+                        placeholderTextColor='rgba(242,244,243, 0.2)'
+                        placeholder='9 cups'>
+                    </TextInput>
 
-                        {/* Weight Goal question and field */}
-                        <Text style={styles.defaultText}>
-                            What is your weight goal?
-                        </Text>
-                        <View style={[{ zIndex: 1 }]}>
-                            <CustomDropdown
-                                placeholder={''}
-                                setCustomValue={setUserWeightGoal}
-                                items={weightGoal}
-                                setItems={setWeightGoal}
-                            />
-                        </View>
-
-                        {/* Water Goal question and field */}
-                        <Text style={styles.defaultText}>
-                            What is your water goal?
-                        </Text>
-                        <TextInput style={styles.inputFieldStyle}
-                            selectionColor='#CB9CF2'
-                            placeholderTextColor='rgba(242,244,243, 0.2)'
-                            placeholder='9 cups'>
-                        </TextInput>
-
-                        {/* Submit Button */}
-                        <View style={{ justifyContent: 'flex-end', paddingTop: 40, paddingBottom: 10 }}>
-                            <TouchableOpacity style={styles.button} onPress={() => router.push('home')}>
-                                <Text style={styles.buttonText}>Continue</Text>
-                            </TouchableOpacity>
-                        </View>
+                    {/* Submit Button */}
+                    <View style={{ justifyContent: 'flex-end', paddingTop: 40, paddingBottom: 10 }}>
+                        <TouchableOpacity style={styles.button} onPress={() => router.push('home')}>
+                            <Text style={styles.buttonText}>Continue</Text>
+                        </TouchableOpacity>
                     </View>
                 </View>
-            </ScrollView>
-        </SafeAreaView>
+            } />
     )
 }
 export default SignUp3;
