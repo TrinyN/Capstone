684d2d751ef0515e765192ef3760448c5b8f9cf9
escott556 <escott1112@outlook.com>
2024-10-05 19:12:47 -0400

    Comment explaining QuestionPage.jsx

---
+++ app/components/QuestionPage.jsx
@@ -0,0 +1,8 @@
+// Is this even needed?
+// This would just end up being the CustomScreen +
+//      a number of questions (that will vary per screen) +
+//      a space and submit button
+//
+// This would require some way to determine and print a certain amount of Q/A's...?
+// Doesn't sound like a good idea --> overcomplicating for minimal return?
+//
