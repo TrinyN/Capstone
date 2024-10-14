06ed82610851fbfe8db5a8c8c12e111e961de037
escott556 <escott1112@outlook.com>
2024-10-14 15:16:52 -0400

    Refactoring index.js, including styling, spacing, and minor component use

--- app/index.jsx
+++ app/index.jsx
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { TouchableOpacity, Text, Image, View, ScrollView, StyleSheet } from 'react-native';
 import { SafeAreaView } from "react-native-safe-area-context";
 import { router } from 'expo-router';
+import CustomButton2 from './components/functional/CustomButton2';
 import styles from './styles';
 
 import {
@@ -35,68 +36,105 @@ export default function App() {
         <SafeAreaView style={styles.container}>
             <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
                 <View style={styles.viewContainer}>
-                    <View style={{ flex: 4}}>
-                        <Text style={[localStyle.welcomeText, { fontSize: 24, textAlign: 'center', paddingTop: 100 }]}>
+
+                    <View style={{flex: 4}}>
+                        <Text style={[localStyle.welcomeText]}>
                             Welcome to
                         </Text>
-                        <Text style={localStyle.welcomeText}>
+                        <Text style={localStyle.logoText}>
                             CaloNavo
                         </Text>
-                        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                        <View style={localStyle.imgView}>
                             <Image
                                 source={require('../assets/images/calonavo-logo.png')}
-                                style={{
-                                    width: '90%',
-                                    height: '90%',
-                                    resizeMode: 'contain',
-                                }}
+                                style={localStyle.logo}
                             />
                         </View>
                     </View>
-                    <View style={{ flex: 1, justifyContent: 'flex-end'}}>
-                        <Text style={[styles.defaultWhiteText, { fontSize: 20, color: '#F2F4F3', paddingHorizontal: 30, paddingBottom: 10, textAlign: 'center' }]}>
+
+                    <View style={{flex: 1, justifyContent: 'flex-end'}}>
+                        <Text style={localStyle.sloganText}>
                             Here to help{' '}
                             <Text style={{color: '#CB9CF2'}}>navogate{' '}</Text>
-                            <Text>
-                                your way to a better diet!
-                            </Text>
+                            <Text>your way to a better diet!</Text>
                         </Text>
                     </View>
-                    <View style={{ flex: 2, paddingTop: 30, paddingBottom: 90, justifyContent: 'center' }}>
+
+                    <View style={localStyle.buttonView}>
                         <Text style={[styles.defaultText, { textAlign: 'center', }]}>
                             Don't have an account yet?
                         </Text>
-
-                        <TouchableOpacity style={styles.button} onPress={() => router.push('/sign-up')}>
-                            <Text style={styles.buttonText}>Get Started</Text>
-                        </TouchableOpacity>
-
-                        <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
-                            <View style={{ flex: 1, height: 1, backgroundColor: '#CB9CF2' }} />
+                        
+                        {/* Signup button */}
+                        <CustomButton2 type='normal' text='Get Started' onPress={() => router.push('/sign-up')}/>
+                        {/* Divider */}
+                        <View style={localStyle.divider}>
+                            <View style={localStyle.line} />
                             <Text style={[styles.defaultText, { marginHorizontal: 10 }]}>
                                 Otherwise
                             </Text>
-                            <View style={{ flex: 1, height: 1, backgroundColor: '#CB9CF2' }} />
+                            <View style={localStyle.line} />
                         </View>
-
-                        <TouchableOpacity style={styles.button} onPress={() => router.push('/sign-in')}>
-                            <Text style={styles.buttonText}>Log In</Text>
-                        </TouchableOpacity>
+                        {/* Signin button */}
+                        <CustomButton2 type='normal' text='Log In' onPress={() => router.push('/sign-in')}/>
                     </View>
                 </View>
-                <StatusBar backgroundColor='#0E1116'
-                    style='light'
-                />
+                {/* Status bar of mobile device */}
+                <StatusBar backgroundColor='#0E1116' style='light'/>
             </ScrollView>
         </SafeAreaView>
     )
 }
 
 const localStyle = StyleSheet.create({
-    welcomeText: {
-        color: '#CB9CF2',
+    logoText: {
+        color: '#CB9CF2',                   // #0E1116
         fontSize: 50,
         textAlign: 'center',
         fontFamily: 'Inter_700Bold',
+        // textShadowColor: '#CB9CF2',      // Options to make background glow
+        // textShadowOffset: {width:0, height:0},
+        // textShadowRadius: 5
     },
+    welcomeText:{                           // Maybe find better font?
+        color: '#CB9CF2',
+        fontSize: 24,
+        textAlign: 'center',
+        fontFamily: 'Inter_700Bold',
+         paddingTop: 100
+    },
+    imgView:{
+        flex: 1, 
+        justifyContent: 'center', 
+        alignItems: 'center' 
+    },
+    logo:{
+        width: '90%', 
+        height: '90%', 
+        resizeMode: 'contain',
+    },
+    sloganText:{
+        fontSize: 20, 
+        color: '#F2F4F3', 
+        paddingHorizontal: 30, 
+        paddingBottom: 10, 
+        textAlign: 'center' ,
+        fontFamily: 'Inter_400Regular',
+    },
+    buttonView:{
+        flex: 2, 
+        paddingTop: 30, 
+        paddingBottom: 90, 
+        justifyContent: 'center'
+    },
+    divider:{
+        flexDirection: 'row', 
+        alignItems: 'center', 
+        justifyContent: 'center' 
+    },
+    line:{
+        flex: 1, 
+        height: 1, 
+        backgroundColor: '#CB9CF2' 
+    }
 })
