f4345a46308b0030d5acf37b4cff7206afc4cbe6
TrinyN <triny.nguyen@g.fmarion.edu>
2024-10-17 15:01:24 -0400

    Revert "Update AddExercise.jsx"
    This reverts commit 9af832e7f7de359191761c9ca25bb9ff0262182b.

--- app/components/functional/AddPopUps/AddExercise.jsx
+++ app/components/functional/AddPopUps/AddExercise.jsx
@@ -4,34 +4,32 @@ import CustomPopUp from '../../structural/CustomPopUp';
 import styles from '../../../styles';
 import CustomDropdown from '../CustomDropdown';
 import { CustomButton } from '../CustomButton';
-import { useExerciseUnitOptions } from '../../../constants/dropdownOptions';
 
-const AddExercise = ({ addExerciseVisible, toggleExerciseOverlay, previousOverlay }) => {
-
-    // Handle dropdown menu options for exervise unit type
-    const { exerciseUnit, setExerciseUnit,
-        exerciseUnitTypes, setExerciseUnitTypes } = useExerciseUnitOptions();
-
+const AddExercise = ({ 
+    addExerciseVisible, toggleExerciseOverlay, 
+    previousOverlay, setExerciseUnit, 
+    exerciseUnitTypes, setExerciseUnitTypes 
+}) => {
     return (
-        <CustomPopUp
-            visible={addExerciseVisible}
-            toggleOverlay={toggleExerciseOverlay}
-            hasBackButton={true}
+        <CustomPopUp 
+            visible={addExerciseVisible} 
+            toggleOverlay={toggleExerciseOverlay} 
+            hasBackButton={true} 
             previousOverlay={previousOverlay}
             content={
                 <View style={localStyle.fieldContainer}>
                     <View style={localStyle.fieldRow}>
-                        <TextInput
-                            style={[styles.inputFieldStyle, { flex: 1 }]}
-                            placeholder='Exercise' selectionColor='#CB9CF2'
+                        <TextInput 
+                            style={[styles.inputFieldStyle, { flex: 1 }]} 
+                            placeholder='Exercise' selectionColor='#CB9CF2' 
                             placeholderTextColor='rgba(242,244,243, 0.2)'>
                         </TextInput>
                     </View>
                     <View style={localStyle.fieldRow}>
                         <View style={{ flex: 1, paddingRight: 10 }}>
-                            <TextInput
-                                style={[styles.inputFieldStyle]}
-                                placeholder='10' selectionColor='#CB9CF2'
+                            <TextInput 
+                                style={[styles.inputFieldStyle]} 
+                                placeholder='10' selectionColor='#CB9CF2' 
                                 placeholderTextColor='rgba(242,244,243, 0.2)'>
                             </TextInput>
                         </View>
@@ -48,10 +46,10 @@ const AddExercise = ({ addExerciseVisible, toggleExerciseOverlay, previousOverla
                         <View style={{ flex: 1, paddingRight: 10 }}>
                             <CustomButton title="Calculate" />
                         </View>
-                        <TextInput
-                            style={[styles.inputFieldStyle, { flex: 1.1 }]}
-                            placeholder='Cals Burned'
-                            selectionColor='#CB9CF2'
+                        <TextInput 
+                            style={[styles.inputFieldStyle, { flex: 1.1 }]} 
+                            placeholder='Cals Burned' 
+                            selectionColor='#CB9CF2' 
                             placeholderTextColor='rgba(242,244,243, 0.2)'>
                         </TextInput>
                     </View>
@@ -64,15 +62,15 @@ const AddExercise = ({ addExerciseVisible, toggleExerciseOverlay, previousOverla
 export default AddExercise;
 
 const localStyle = StyleSheet.create({
-    fieldContainer: {
-        paddingHorizontal: 30,
-        paddingBottom: 20,
+    fieldContainer:{
+        paddingHorizontal: 30, 
+        paddingBottom: 20, 
         justifyContent: 'center'
     },
-    fieldRow: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
+    fieldRow:{
+        flexDirection: 'row', 
+        justifyContent: 'space-between', 
+        alignItems: 'center', 
         paddingTop: 20
     }
 })
