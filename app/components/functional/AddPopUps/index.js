c5d85a03a133396ecbfce38ca43340f9508c3ed3
TrinyN <triny.nguyen@g.fmarion.edu>
2024-10-17 15:00:59 -0400

    Revert "Update index.js"
    This reverts commit 90d8c4ba06d610c521599512141cb64aa0f1906f.

--- app/components/functional/AddPopUps/index.js
+++ app/components/functional/AddPopUps/index.js
@@ -1,4 +1,4 @@
 export { default as AddWater } from './AddWater';
 export { default as AddExercise } from './AddExercise';
 export { default as AddPopUp } from './AddPopUp';
-export { default as AddFood } from './AddFood';
+// export { default as AddFood } from './AddFood';
